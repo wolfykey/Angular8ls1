23444b28b4ca8e80229c9d79e93b2e3772d9ee4c
Vlad Vovk <wolfykey@gmail.com>
2020-02-16 22:34:46 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,4 +24,8 @@ export class AppComponent {
       id: 2
     },
   ];
+
+  updatePosts(post: Post) {
+    this.posts.unshift(post);
+  }
 }
--- src/app/post-form/post-form.component.ts
+++ src/app/post-form/post-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Post} from '../app.component';
 
 @Component({
   selector: 'app-post-form',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-form.component.scss']
 })
 export class PostFormComponent implements OnInit {
+  @Output() onAdd: EventEmitter<Post> = new EventEmitter<Post>();
+
+  title = '';
+  text = '';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addPost() {
+    if (this.title.trim() && this.text.trim()) {
+      const post: Post = {
+        title: this.title,
+        text: this.text
+      }
+
+      console.log(post);
+      this.onAdd.emit(post);
+
+      this.text = this.title = '';
+    }
   }
 
 }
