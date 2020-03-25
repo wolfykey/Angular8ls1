74f77950a41fee078d01b4bcd46ae8b8d05b2a68
Vlad Vovk <wolfykey@gmail.com>
2020-03-25 13:37:02 +0200

    custom pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent {
-  e: number = Math.E;
-  str: string = "hello world";
-  date: Date = new Date();
-  float = 0.42;
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from "./app.component";
+import { AppComponent } from './app.component';
+import { MultByPipe } from './pipes/mult-by.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MultByPipe],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
