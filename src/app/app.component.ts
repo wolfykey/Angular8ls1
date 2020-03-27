4bef4ee80c05d5f58badb45c8976f97f008d7a59
Vlad Vovk <wolfykey@gmail.com>
2020-03-27 12:28:23 +0200

    add reactive forms validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -18,7 +18,12 @@ export class AppComponent implements OnInit {
       password: new FormControl(null, [
         Validators.required,
         Validators.minLength(6)
-      ])
+      ]),
+      address: new FormGroup({
+        country: new FormControl('ua'),
+        city: new FormControl('', Validators.required)
+      }),
+      skills: new FormArray([])
     });
   }
 
@@ -28,5 +33,26 @@ export class AppComponent implements OnInit {
       const formData = { ...this.form.value };
       console.log('Form data: ', formData);
     }
+  }
+
+  setCapital() {
+    const cityMap = {
+      ua: 'Kiev',
+      by: 'Minsk',
+      kz: 'Astana'
+    }
+
+    const cityKey = this.form.get('address').get('country').value;
+    const city = cityMap[cityKey];
+
+    this.form.patchValue({
+      address: { city }
+    });
+  }
+
+  addSkill() {
+    const control = new FormControl('', Validators.required);
+    // (<FormArray>this.form.get('skills'))
+    (this.form.get('skills') as FormArray).push(control);
   }
 }
